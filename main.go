79e2ec7d3338e324495716bd6c2fed30e15ac50e
Samuel Adetunji <guysnacho2@gmail.com>
2020-07-21 11:01:49 -0500

    Book update works but its buggy

--- main.go
+++ main.go
@@ -58,7 +58,20 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 //Update A Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //Get Parameters
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000000)) //Mock ID - not safe, could make duplicates
+			books = append(books, book)
+			json.NewEncoder(w).Encode(&Book{})
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 //Delete A Book
