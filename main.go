359c4d390c85a36033a07ca05014e2508b0c2577
Samuel Adetunji <guysnacho2@gmail.com>
2020-07-21 10:40:56 -0500

    Added book post request

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -46,7 +48,12 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 //Create A Book
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000)) //Mock ID - not safe, could make duplicates
+	books = append(books, book)
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 //Update A Book
